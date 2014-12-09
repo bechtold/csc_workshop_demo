99cf7138501a634105436dc1001b6009a772a34a
Oskar Bechtold <bechtold@2008118.no-reply.drupal.org>
2014-12-09 17:08:33 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/**
+ * User: oskar
+ * Date: 22.11.14
+ * Time: 13:31
+ */
+
+$(function () {
+
+  $(window).bind('hashchange', function(e){
+    $('html').attr('class', window.location.hash.replace('#',''));
+  });
+
+  $(window).trigger('hashchange');
+});
